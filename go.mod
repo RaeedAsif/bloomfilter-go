f231fc8cbaeaeb0ebc8b3175a31a87bcfddce966
Raeed Asif <raeedasif1@gmail.com>
2022-11-21 10:56:17 +0500

    added env

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require github.com/gorilla/mux v1.8.0
 
-require github.com/spaolacci/murmur3 v1.1.0
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/spaolacci/murmur3 v1.1.0
+)
--- main.go
+++ main.go
@@ -9,12 +9,19 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/joho/godotenv"
+
 	"github.com/RaeedAsif/flare-go-test/routes"
 	"github.com/RaeedAsif/flare-go-test/storage"
 )
 
 func main() {
-	PORT := "8080"
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	PORT := os.Getenv("PORT")
 
 	var wait time.Duration
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
@@ -29,7 +36,7 @@ func main() {
 	log.Println("routers intiliased")
 
 	srv := &http.Server{
-		Addr: "0.0.0.0:8080",
+		Addr: "0.0.0.0:" + PORT,
 		// Good practice to set timeouts to avoid Slowloris attacks.
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
@@ -44,7 +51,7 @@ func main() {
 		}
 	}()
 
-	log.Println("listening http server start on " + string(PORT))
+	log.Println("listening http server start on :" + string(PORT))
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
